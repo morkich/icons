37673dc69df251429fc9e69e592fc482eb63e203
morkich <morkich1@gmail.com>
2025-10-21 17:35:39 +0300

    фильтрация теперь работает.

--- src/stores/iconsStore.js
+++ src/stores/iconsStore.js
@@ -13,12 +13,11 @@ export const filterIconsBySearchQueryThunk = ({query, iconDataInStore}) => {
         return false;
     }
 
-    const filteredKeys = Object.keys(iconDataInStore).filter(iconKey => {
+    const filteredIndexes = Object.keys(iconDataInStore).filter(iconKey => {
         const tags = iconDataInStore[iconKey].tags;
         return tags.some(item => item.includes(query));
     })
 
-    let result = {};
-    filteredKeys.forEach(key => result = {...result, [key]: iconDataInStore[key]});
+    const result = filteredIndexes.map(index => iconDataInStore[index]);
     iconsDataDisplayStore.set(result);
 }
